19ca63ca1c39f88836b952536da1f5bec8b6394c
ThamizhpriyanM <thamizhpriyan.official@gmail.com>
2024-05-10 16:41:10 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+var typed = new Typed('.text', {
+    strings: ['Backend Developer', 'Web Developer'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    smartBackspace: true,
+    loop:true
+  });
